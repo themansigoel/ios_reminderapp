e2313284ea0ecf894bf1e9387b548d1bf6c033f9
Mansi Agarwal <-->
2020-09-13 21:55:17 +0530

    Notification Scheduling changes

--- Reminder/CreateNewReminderViewController.swift
+++ Reminder/CreateNewReminderViewController.swift
@@ -10,22 +10,14 @@ import UIKit
 class CreateNewReminderViewController: UIViewController {
 
     @IBOutlet weak var txtTitle: UITextField!
+    @IBOutlet weak var txtDescription: UITextField!
     @IBOutlet weak var btnSelectTime: UIButton!
-    
-    @IBAction func selectTimePressed(_ sender: Any) {
-    }
-    
-    
-    @IBAction func createReminder(_ sender: Any) {
-    }
-    
+    @IBOutlet weak var datePicker: ScheduleTimePicker!
     
     let notifmanager = LocalNotificationManager()
 
-    @IBOutlet weak var txtDescription: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        scheduleNotification()
     }
     
     class func newInstance()->CreateNewReminderViewController{
@@ -34,8 +26,57 @@ class CreateNewReminderViewController: UIViewController {
     }
     
     func scheduleNotification(){
-        notifmanager.getUserPermissionForNotification()
+        notifmanager.getUserPermissionForNotification(completion: {[weak self]
+            (success, error) in
+            if success{
+                if let title = self?.txtTitle.text, let description = self?.txtDescription.text{
+                    self?.notifmanager.setNotfication(title: title, desc: description, time: 1)
+                    
+                }
+            }
+        })
     }
     
+    @IBAction func selectTimePressed(_ sender: Any) {
+    }
+    
+    @IBAction func createReminder(_ sender: Any) {
+        validateData()
+    }
+    
+    
+    @IBAction func dateChanged(_ sender: Any) {
+
+    }
+    
+}
+
+extension CreateNewReminderViewController{
+    func validateData(){
+          //Title should not be empty
+        if (txtTitle.text?.isEmpty ?? true){
+            showAlert(message: "Please Enter title", title: "Required")
+        }
+        //Desc should not be empty
+        else if (txtDescription.text?.isEmpty ?? true){
+                   showAlert(message: "Please Enter description", title: "Required")
+               }
+        //Time should be set
+        else if (btnSelectTime.titleLabel?.text == "Select Time"){
+            showAlert(message: "Please Enter Valid Time", title: "Required")
+        }else{
+            //All Validation done create Reminder
+            scheduleNotification()
+        }
+    }
+    
+    func showErrorValidationAlert(str:String){
+        
+    }
+    func showAlert(message:String, title:String){
+        let  alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
 
 }
--- Reminder/LocalNotificationManager.swift
+++ Reminder/LocalNotificationManager.swift
@@ -10,25 +10,26 @@ import UserNotifications
 
 class LocalNotificationManager{
     
-    func getUserPermissionForNotification(){
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { [weak self] success, error in
-            if success {
-                self?.setNotfication(title: "Hi", desc: "Mansi is Cool", time: 2.0)
-                print("All set!")
-            } else if let error = error {
-                print(error.localizedDescription)
+    
+    func getUserPermissionForNotification(completion: @escaping (Bool , Error?)-> Void){
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+            DispatchQueue.main.async {
+                completion(success, error)
+            }
             }
         }
-    }
-    
+
     func setNotfication(title: String, desc:String, time:TimeInterval){
         let content = UNMutableNotificationContent()
         content.title = title
         content.subtitle = desc
         content.sound = UNNotificationSound.default
 
-        // show this notification five seconds from now
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        var dateComponents = DateComponents()
+        dateComponents.hour = 10
+        dateComponents.minute = 30
+
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
 
         // choose a random identifier
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
